feb28866df97faa49cf10eb56c4fc1c1627af605
Sergio Perez <sergio.perezbaron@kellogg.northwestern.edu>
2023-05-22 02:48:36 +0000

    commit #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources "places"
+  resources "posts"
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
 end
