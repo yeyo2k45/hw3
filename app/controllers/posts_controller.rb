d3a86a2f5d6a2a6f025ac03d4a0c7e50e5bf7211
Sergio Perez <sergio.perezbaron@kellogg.northwestern.edu>
2023-05-22 03:00:43 +0000

    commit #7

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,28 @@
 class PostsController < ApplicationController
+
+
+  def create
+  @post = Post.new
+    
+    @post["name"] = params["post"]["name"]
+    @post["date"] = params["post"]["date"]
+    @post["description"] = params["post"]["description"]
+    
+    @post["place_id"] = params["post"]["place_id"]
+    @post.save
+    
+    redirect_to "/places/#{@post["place_id"]}"
+  end
+
+  def new
+
+    @post = Post.new
+    @post["place_id"] = params["place_id"]
+      end
+  def show
+  
+    @post = Post.find_by({"id" => params["id"]})
+    @place = Place.find_by({"id" => @post["place_id"]})
+  
+  end
 end
